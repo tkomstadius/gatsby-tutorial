a5f6b3fcdd9d77ad62ce8da1c5fc5a298a9dd5c7
Therese Komstadius <therese.komstadius@mpyadigital.com>
2020-02-18 11:04:33 +0100

    gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,41 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Pandas Eating Lots',
+    description: 'A simple description about this hello world site',
+    author: 'tkomstadius',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        // Enables "Add to Homescreen" promt and disables browser UI (including back butotn)
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+  ],
 }
---
+++ src/pages/my-blog-post.md
@@ -0,0 +1,7 @@
+---
+title: "This is my new blog post"
+date: "2020-02-17"
+---
+
+
+I have some content here but I don't know anything so this might be it. 
